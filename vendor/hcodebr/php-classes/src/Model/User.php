8a47b2f57f1f06985bd98d77d661614fb72c092e
sgrsilverio <shalongushat@gmail.com>
2018-08-09 16:33:49 -0300

    Primeiro commit!

---
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Hcode\Model;
+
+use \Hcode\Model;
+use \Hcode\DB\Sql;
+
+class User extends Model {
+
+    const SESSION = "User";
+
+    protected $fields = [
+        "iduser", "idperson", "deslogin", "despassword", "inadmin", "dtergister"
+    ];
+
+    public static function login($login, $password):User
+    {
+
+        $db = new Sql();
+
+        $results = $db->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
+            ":LOGIN"=>$login
+        ));
+
+        if (count($results) === 0) {
+            throw new \Exception("Não foi possível fazer login.");
+        }
+
+        $data = $results[0];
+
+        if (password_verify($password, $data["despassword"])) {
+
+            $user = new User();
+            $user->setData($data);
+
+            $_SESSION[User::SESSION] = $user->getValues();
+
+            return $user;
+
+        } else {
+
+            throw new \Exception("Não foi possível fazer login.");
+
+        }
+
+    }
+
+    public static function logout()
+    {
+
+        $_SESSION[User::SESSION] = NULL;
+
+    }
+
+    public static function verifyLogin($inadmin = true)
+    {
+
+        if (
+            !isset($_SESSION[User::SESSION])
+            ||
+            !$_SESSION[User::SESSION]
+            ||
+            !(int)$_SESSION[User::SESSION]["iduser"] > 0
+            ||
+            (bool)$_SESSION[User::SESSION]["iduser"] !== $inadmin
+        ) {
+
+            header("Location: /admin/login");
+            exit;
+
+        }
+
+    }
+
+    public static function listAll(){
+        $sql = new sql();
+        return $sql->select("select * from tb_users a INNER JOIN tb_persons b USING (idperson) ORDER BY b.desperson");
+    }
+
+    /**
+     *
+     */
+    public function save($newuser = array()){
+        $sql = new Sql();
+        $sql->select("CALL sp_users_save (:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+            ":desperson"=>$newuser["desperson"],
+            ":deslogin"=>$newuser["deslogin"],
+            ":despassword"=>$newuser["despassword"],
+            ":desemail"=>$newuser["desemail"],
+            ":nrphone"=>$newuser["nrphone"],
+            ":inadmin"=>$newuser["inadmin"]
+
+        ));
+
+        //$this->setData($results[0]);
+
+
+
+
+
+    }
+}
+
+?>
