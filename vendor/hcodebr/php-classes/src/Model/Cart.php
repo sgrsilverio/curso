b043af320b6137d0e99aae764510e627c708c3fe
sgrsilverio <shalongushat@gmail.com>
2018-09-08 22:57:29 -0300

    Carrinho aula 1

--- vendor/hcodebr/php-classes/src/Model/Cart.php
+++ vendor/hcodebr/php-classes/src/Model/Cart.php
@@ -14,28 +14,27 @@ use \Hcode\Model\User;
 
 
 class Cart extends Model {
-
     const SESSION = "Cart";
 
-    public static function getFromSession(){
+
+    public static function getFromSession()
+    {
         $cart = new Cart();
         if (isset($_SESSION[Cart::SESSION]) && (int)$_SESSION[Cart::SESSION]['idcart'] > 0) {
             $cart->get((int)$_SESSION[Cart::SESSION]['idcart']);
         } else {
             $cart->getFromSessionID();
-            if (!(int)$cart->getidcart() > 0 ){
+            if (!(int)$cart->getidcart() > 0) {
                 $data = [
                     'dessessionid'=>session_id()
                 ];
-                if (User::checkLogin(false)){
+                if (User::checkLogin(false)) {
                     $user = User::getFromSession();
                     $data['iduser'] = $user->getiduser();
                 }
                 $cart->setData($data);
-
                 $cart->save();
                 $cart->setToSession();
-
             }
         }
         return $cart;
@@ -47,7 +46,7 @@ class Cart extends Model {
 
     public function getFromSessionID(){
         $sql = new Sql();
-        $results = $sql->select("SELECT * FROM tb_carts WHERE dessessionid = :dessessionid",[
+        $results = $sql->select("SELECT * FROM tb_carts WHERE dessessionid = :dessessionid", [
             ':dessessionid'=>session_id()
         ]);
 
@@ -56,30 +55,30 @@ class Cart extends Model {
         }
     }
 
-
-    public function save(){
+    public function get(int $idcart)    {
         $sql = new Sql();
-        $sql->select("CALL sp_carts_save(:pidcart, :pdessessionid, :piduser, :pdeszipcode, :pvlfreight, :pnrdays)" , [
-            ':pidcart'=>$this->getidcart,
-            ':pdessessionid'=>$this->getdessessionid,
-            ':piduser'=>$this->getiduser,
-            ':pdeszipcode'=>$this->getdeszipcode,
-            ':pvlfreight'=>$this->getvlfreight,
-            ':pnrdays'=>$this->getnrdays
-
-        ]);
-
-    }
-
-    public function get(int $idcart) {
-        $sql = new Sql();
-        $results = $sql->select(" SELECT * FROM tb_carts WHERE idcart = :idcart", [
-           ':idcart'=>$idcart
+        $results = $sql->select("SELECT * FROM tb_carts WHERE idcart = :idcart", [
+            ':idcart'=>$idcart
         ]);
         if (count($results) > 0) {
             $this->setData($results[0]);
         }
-
     }
+
+    public function save()
+    {
+        $sql = new Sql();
+        $results = $sql->select("CALL sp_carts_save(:idcart, :dessessionid, :iduser, :deszipcode, :vlfreight, :nrdays)", [
+            ':idcart'=>$this->getidcart(),
+            ':dessessionid'=>$this->getdessessionid(),
+            ':iduser'=>$this->getiduser(),
+            ':deszipcode'=>$this->getdeszipcode(),
+            ':vlfreight'=>$this->getvlfreight(),
+            ':nrdays'=>$this->getnrdays()
+        ]);
+        $this->setData($results[0]);
+    }
+
+
 
 }
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -76,7 +76,7 @@ class User extends Model {
 
     public static function verifyLogin($inadmin = true)
     {
-        if (User::checkLogin($inadmin)) {
+        if (!User::checkLogin($inadmin)) {
             header("Location: /admin/login");
             exit;
         }
