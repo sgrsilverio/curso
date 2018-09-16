93d13ca0082f98564312409b663539ed3834cafc
sgrsilverio <shalongushat@gmail.com>
2018-09-15 22:38:44 -0300

    CarrinhoFrete

--- index.php
+++ index.php
@@ -346,7 +346,8 @@ $app->get("/cart", function (){
     $page = new Page();
     $page->setTpl("cart", [
         'cart'=>$cart->getValues(),
-        'products'=>$cart->getProducts()
+        'products'=>$cart->getProducts(),
+        'error'=>Cart::getMsgError()
     ]);
 });
 
@@ -358,7 +359,7 @@ $app->get("/cart/:idproduct/add", function ($idproduct){
     for ($i = 0; $i < $qtd; $i++) {
         $cart->addProduct($product);
     }
-    
+
     header("Location: /cart");
     exit;
 });
@@ -381,7 +382,12 @@ $app->get("/cart/:idproduct/remove", function ($idproduct){
     exit;
 });
 
-
+$app->post("/cart/freight", function (){
+   $cart = Cart::getFromSession();
+   $cart->setFreight($_POST['zipcode']);
+   header("Location: /cart");
+   exit;
+});
 
 
 $app->run();
